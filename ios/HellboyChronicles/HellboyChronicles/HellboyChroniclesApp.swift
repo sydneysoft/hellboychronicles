a10a8b97d8eabbcfe25f3334dbd17cd6cc4bc9f5
sydneysoft <60692141+sydneysoft@users.noreply.github.com>
2026-09-08 16:24:00 +0100

    Show audiobook player across native reading modes

--- ios/HellboyChronicles/HellboyChronicles/HellboyChroniclesApp.swift
+++ ios/HellboyChronicles/HellboyChronicles/HellboyChroniclesApp.swift
@@ -3,12 +3,19 @@ import SwiftUI
 @main
 struct HellboyChroniclesApp: App {
     @StateObject private var appState = AppState()
+    @StateObject private var readingContext = ReadingContextStore.shared
 
     var body: some Scene {
         WindowGroup {
-            RootView()
-                .environmentObject(appState)
-                .preferredColorScheme(.dark)
+            ZStack(alignment: .bottom) {
+                RootView()
+
+                GlobalStoryAudioBar(context: readingContext)
+                    .padding(.bottom, 58)
+                    .zIndex(10)
+            }
+            .environmentObject(appState)
+            .preferredColorScheme(.dark)
         }
     }
 }
