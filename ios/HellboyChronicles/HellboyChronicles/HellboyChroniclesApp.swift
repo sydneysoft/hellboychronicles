d6d9790a50351ee925d0756a26179042ddbc090a
sydneysoft <60692141+sydneysoft@users.noreply.github.com>
2026-09-08 15:01:00 +0100

    Add SwiftUI app entry point

---
+++ ios/HellboyChronicles/HellboyChronicles/HellboyChroniclesApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+@main
+struct HellboyChroniclesApp: App {
+    @StateObject private var appState = AppState()
+
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .environmentObject(appState)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
