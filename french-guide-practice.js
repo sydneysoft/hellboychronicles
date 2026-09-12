894b882f1043b992356e41e441cda3f3f658057d
sydneysoft <60692141+sydneysoft@users.noreply.github.com>
2026-09-12 13:56:08 +0100

    Apply block theme to French study guides

--- french-guide-practice.js
+++ french-guide-practice.js
@@ -1,3 +1,4 @@
+(()=>{document.documentElement.classList.add('storylingo-block-rest');if(!document.querySelector('link[data-storylingo-block-rest]')){const l=document.createElement('link');l.rel='stylesheet';l.href='/storylingo-block-rest.css?v=20260912-2';l.dataset.storylingoBlockRest='1';document.head.appendChild(l)}if(!document.querySelector('script[data-storylingo-cookie]')){const s=document.createElement('script');s.src='/storylingo-cookie.js?v=20260912-2';s.dataset.storylingoCookie='1';document.body.appendChild(s)}})();
 (()=>{
 const norm=s=>(s||'').toLocaleLowerCase('fr-FR').normalize('NFD').replace(/[\u0300-\u036f]/g,'').replace(/[’']/g,"'").replace(/[^a-z0-9' ]/g,' ').replace(/\s+/g,' ').trim();
 const loose=s=>norm(s).replace(/'/g,'');
